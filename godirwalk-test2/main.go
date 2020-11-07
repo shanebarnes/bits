30bd2f8767adcd2414a1567dd52b4ee707bfa9c4
shanebarnes <shanebarnes@users.noreply.github.com>
2020-11-07 08:44:16 -0500

    Add reporting on a fixed time interval

--- godirwalk-test2/main.go
+++ godirwalk-test2/main.go
@@ -9,18 +9,33 @@ import (
 
 func main() {
 	dir := flag.String("dir", "", "directory to walk")
+	reportInt := flag.Int("report", 15, "report interval in seconds")
 	flag.Parse()
+
+	if *reportInt < 1 {
+		panic("Report interval must be greater than zero")
+	}
+	reportDur := time.Duration(*reportInt) * time.Second
+
 	fmt.Println("Starting walk of", *dir)
 	cbCount := 0
 	t0 := time.Now()
+	tr := t0
+
 	godirwalk.Walk(*dir, &godirwalk.Options{
 		Callback: func(osPathname string, de *godirwalk.Dirent) error {
 			cbCount++
+			lastReport := time.Since(tr)
+			if lastReport > reportDur {
+				tr = tr.Add(reportDur)
+				fmt.Println("Found", cbCount, "files in", time.Since(t0))
+			}
 			return nil
 		},
 		Unsorted: true, // (optional) set true for faster yet non-deterministic enumeration (see godoc)
 	})
+
 	t1 := time.Now()
-	fmt.Println("Found", cbCount, "files in", t1.Sub(t0))
+	fmt.Println("Found a total of", cbCount, "files in", t1.Sub(t0))
 	fmt.Println("Completed walk of", *dir)
 }
